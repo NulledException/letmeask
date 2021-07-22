e190b6c99a4a46c3c73fced127c66bbae4723023
NulledException <51205460+NulledException@users.noreply.github.com>
2021-07-21 22:06:50 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/rooms/new" exact component={NewRoom}></Route>
+          <Route path="/rooms/:id" exact component={Room}></Route>
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
